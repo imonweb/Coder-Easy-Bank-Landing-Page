45e70eb9bb090d36d7686aa7806d71a07822ff77
imonweb <contact@imonweb.co.uk>
2021-11-26 17:12:01 +0000

    animated hamburger menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,18 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 
 btnHamburger.addEventListener('click', () => {
 
-  if(btnHamburger.classList.contains('open')){
-    btnHamburger.classList.remove('open');
+  if(header.classList.contains('open')){
+    /*  Close Hamburger Menu */
+    header.classList.remove('open');
+    overlay.classList.remove('fade-in');
+    overlay.classList.add('fade-out');
   } else {
-    btnHamburger.classList.add('open');
+    /*  Open Hamburger Menu */
+    header.classList.add('open');
+    overlay.classList.remove('fade-out');
+    overlay.classList.add('fade-in');
   }
 })
